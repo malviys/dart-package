f099b1211cb1a3ac5b203da72bcd5f72b003a30a
Sourabh Malviya <saurabhmalvia997@gmail.com>
2021-02-23 23:12:47 +0530

    moving assertions to assertions.dart

---
+++ dex_generator/lib/src/utils/assertions.dart
@@ -0,0 +1,19 @@
+import 'package:analyzer/dart/element/element.dart';
+
+void assertDataClass(Element element) {
+  Element el;
+
+  try {
+    el = element as ClassElement;
+  } catch (e) {
+    throw Exception(
+      "${element.displayName} should be a class check your definition at ${element.library}",
+    );
+  }
+
+  if (!el.isPrivate && !el.displayName.startsWith("_\$")) {
+    throw Exception(
+      "${el.displayName} should be private and starts with _\$ check your definition at ${el.library}",
+    );
+  }
+}
